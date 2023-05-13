b5ea6d88a74f04f4db9265d9be75daf86b2b4717
anchous <leonovivan171@gmail.com>
2023-05-13 16:59:06 +0300

    scaling sprites of objects - added

--- main.py
+++ main.py
@@ -14,19 +14,19 @@ screen = pygame.display.set_mode(W_SIZE, pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 FPS = 60
 
-rotate_speed = 365
+rotate_speed = 365 / 3
 length = 1
 
-sun = Object(screen, (253, 184, 19), 5)
-earth = MovingObject(screen, (0, 184, 34), 3, length * 151, rotate_speed / 365, sun)
-mars = MovingObject(screen, (253, 104, 19), 3, length * 250, rotate_speed / 687, sun)
-moon = MovingObject(screen, (200, 200, 200), 2, length * 40, rotate_speed / 30, earth)
+sun = Object(screen, 5, "data/sun.png")
+earth = MovingObject(screen, 4, "data/earth.png", length * 151, rotate_speed / 365, sun)
+mars = MovingObject(screen, 4, "data/mars.png", length * 250, rotate_speed / 687, sun)
+moon = MovingObject(screen, 3, "data/moon.png", length * 40, rotate_speed / 30, earth)
 
-objects = Objects((H_WIDTH, H_HEIGHT), sun, mars, moon, earth)
+objects = Objects((H_WIDTH, H_HEIGHT), sun, earth, sun, mars, moon)
 
 mouse_pos = mx, my = 0, 0
 is_drag = False
-sacle_factor = 1.1
+scale_factor = 1.1
 
 while True:
     screen.fill((0, 0, 0))
@@ -49,9 +49,9 @@ while True:
             if event.button == 1:
                 clicked = True
             if event.button == 4:
-                objects.scale(sacle_factor)
+                objects.scale(scale_factor)
             if event.button == 5:
-                objects.scale(1 / sacle_factor)
+                objects.scale(1 / scale_factor)
 
         if event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
@@ -59,8 +59,9 @@ while True:
 
     if clicked:
         is_drag = True
-
+    
     objects.update()
+
     # print(clock.get_fps())
     pygame.display.update()
     clock.tick(FPS)
--- space_objects.py
+++ space_objects.py
@@ -1,44 +1,67 @@
 import pygame
 
 from math_tools import *
+from tools import *
 
 
 class Object:
-    def __init__(self, screen: pygame.Surface, color: tuple, radius: int | float):
+    def __init__(self, screen: pygame.Surface, radius: int | float, filepath: str):
         self.screen = screen
         self.radius = radius
 
-        self.image = pygame.Surface((radius * 2, radius * 2))
-        pygame.draw.circle(self.image, color, (radius, radius), radius)
-        self.image.set_colorkey((0, 0, 0))
+        self.image = load_image(filepath)
+        size = self.image.get_width()
+
+        self.scale_factor = radius / (size / 2)
 
         self.scale = 1
 
         self.trajectory = []
         self.trajectory_points_limit = 1000
 
+        self.trajectory_color = get_trajectory_color(self.image)
+
+        self.current_image = self.get_scaled_image()
+
     def update(self):
-        if len(self.trajectory) > self.trajectory_points_limit:
+        if len(self.trajectory) >= self.trajectory_points_limit:
             del self.trajectory[: -self.trajectory_points_limit]
 
     def draw(self, offset_x, offset_y, x, y):
-        res_x, res_y = (x, y)
-        self.trajectory.append((res_x, res_y))
-        self.screen.blit(
-            self.image, (res_x + offset_x - self.radius, res_y + offset_y - self.radius)
+        self.trajectory.append((x, y))
+        scaled_radius = self.radius * self.scale
+        coords = (
+            x + offset_x - scaled_radius,
+            y + offset_y - scaled_radius,
         )
+        self.screen.blit(
+            self.current_image,
+            coords,
+        )
+    
+    def get_scaled_image(self):
+        img = pygame.transform.scale(
+            self.image,
+            (
+                self.image.get_width() * self.scale_factor * self.scale,
+                self.image.get_height() * self.scale_factor * self.scale,
+            ),
+        )
+        return img
 
     def get_offsets(self):
         return 0, 0
 
     def set_scale(self, scale_factor):
-        self.scale = scale_factor
+        if self.scale != scale_factor:
+            self.current_image = self.get_scaled_image()
+            self.scale = scale_factor
 
     def draw_trajectory(self, x_offset, y_offset):
         if len(self.trajectory) > 1:
             comparator = lambda coords: (coords[0] + x_offset, coords[1] + y_offset)
             points = tuple(map(comparator, self.trajectory))
-            pygame.draw.aalines(self.screen, (200, 200, 200), False, points)
+            pygame.draw.aalines(self.screen, self.trajectory_color, False, points)
 
     def clear_trajectory(self):
         self.trajectory.clear()
@@ -48,22 +71,22 @@ class MovingObject(Object):
     def __init__(
         self,
         screen: pygame.Surface,
-        color: tuple,
         radius: int | float,
+        filepath: str,
         movement_radius: int | float,
         angle: int | float,
         main_object: Object,
     ):
-        super().__init__(screen, color, radius)
+        super().__init__(screen, radius, filepath)
         self.movement_radius = movement_radius
         self.angle = angle
         self.main_object = main_object
 
         self.vec = Vector(-self.movement_radius, 0)
 
-        self.image = pygame.Surface((radius * 2, radius * 2))
-        pygame.draw.circle(self.image, color, (radius, radius), radius)
-        self.image.set_colorkey((0, 0, 0))
+        self.image = load_image(filepath)
+
+        self.current_image = self.get_scaled_image()
 
     def update(self):
         if len(self.trajectory) >= self.trajectory_points_limit:
@@ -76,8 +99,19 @@ class MovingObject(Object):
         self.trajectory.append((res_x, res_y))
 
         self.screen.blit(
-            self.image, (res_x + offset_x - self.radius, res_y + offset_y - self.radius)
+            self.current_image,
+            (res_x + offset_x - self.radius * self.scale, res_y + offset_y - self.radius * self.scale),
         )
+    
+    def get_scaled_image(self):
+        img = pygame.transform.scale(
+            self.image,
+            (
+                self.image.get_width() * self.scale_factor * self.scale,
+                self.image.get_height() * self.scale_factor * self.scale,
+            ),
+        )
+        return img
 
     def get_offsets(self):
         x, y = self.main_object.get_offsets()
@@ -88,7 +122,7 @@ class MovingObject(Object):
         if len(self.trajectory) > 1:
             comparator = lambda coords: (coords[0] + x_offset, coords[1] + y_offset)
             points = tuple(map(comparator, self.trajectory))
-            pygame.draw.aalines(self.screen, (200, 200, 200), False, points)
+            pygame.draw.aalines(self.screen, self.trajectory_color, False, points)
 
 
 class Camera:
@@ -101,8 +135,8 @@ class Camera:
         self.camera_offset_x, self.camera_offset_y = 0, 0
 
     def __call__(self, obj):
-        obj.draw(self.x_offset, self.y_offset, self.x, self.y)
         obj.draw_trajectory(self.x_offset, self.y_offset)
+        obj.draw(self.x_offset, self.y_offset, self.x, self.y)
 
     def update(self):
         self.x, self.y = self.main_object.get_offsets()
@@ -124,16 +158,18 @@ class Objects:
         self.camera = Camera(self.objects[0], offsets)
         self.zoom = 1
         self.min_zoom = 0.01
-        self.max_zoom = 100
+        self.max_zoom = 40
 
     def update(self):
+        for obj in self.objects:
+            obj.set_scale(self.zoom)
+        
         for obj in self.objects:
             obj.update()
 
         self.camera.update()
 
-        for obj in self.objects:
-            obj.set_scale(self.zoom)
+        
 
         for obj in self.objects:
             self.camera(obj)
